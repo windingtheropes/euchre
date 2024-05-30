7440e6a1155cf8933e4bf66b3dcc1626086c39ba
windingtheropes <jaandersonck@gmail.com>
2024-05-29 22:09:35 -0400

    add graphics sketches

--- graphics.py
+++ graphics.py
@@ -3,9 +3,26 @@ import pygame
 pygame.init()
 
 
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((1536,864))
 clock = pygame.time.Clock()
+pygame.display.set_caption("Euchre")
 
-while True:
+
+
+img = pygame.image.load("img/jack_of_hearts.png").convert()
+# dimensions of any given card is 222x323, so this is half scale
+img = pygame.transform.scale(img, (222/2,323/2))
+running = True
+while running:
+    # the active screen could be a class, which has bindings to events, and has a render method.
+     # activeScreen = 
+    for event in pygame.event.get():
+        # close window if pressed close
+        if event.type == pygame.QUIT:
+            running = False
+        
+    screen.blit(img, (50,0))
     pygame.display.flip()
     clock.tick(60)
+else:
+    pygame.quit()
